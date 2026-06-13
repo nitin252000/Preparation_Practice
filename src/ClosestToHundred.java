dea5d413f4aa5b4f00817339bd233774559c5a7c
nitin252000 <sknsingh123@gmail.com>
2026-06-13 09:53:20 +0530

    "ClosestToHundred"

---
+++ src/ClosestToHundred.java
@@ -0,0 +1,37 @@
+public class ClosestToHundred {
+
+    public static void main(String[] args) {
+
+        int[] ar = {95, 120, 85, 105, 98, 102, 150};
+
+        int minDiff = Integer.MAX_VALUE;
+
+        // Find minimum difference
+        for (int num : ar) {
+            minDiff = Math.min(minDiff, Math.abs(num - 100));
+        }
+
+        // Print all numbers with minimum difference
+        System.out.print("Closest numbers: ");
+        for (int num : ar) {
+            if (Math.abs(num - 100) == minDiff) {
+                System.out.print(num + " ");
+            }
+        }
+
+        System.out.println();
+        System.out.println("One closest number: " + closestTo100(ar));
+    }
+
+    public static int closestTo100(int[] arr) {
+        int closest = arr[0];
+
+        for (int num : arr) {
+            if (Math.abs(num - 100) < Math.abs(closest - 100)) {
+                closest = num;
+            }
+        }
+
+        return closest;
+    }
+}
